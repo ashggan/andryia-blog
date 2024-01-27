1712502460d0ee27cfa7579ec719739237e53c3c
Ashgan <ashganwiki@gmail.com>
2024-01-27 02:57:17 +0200

    bug fix : translationa

--- components/Card.vue
+++ components/Card.vue
@@ -5,7 +5,7 @@
                 class="card card-side rounded-none border-b-2 mb-3 border-b-indigo-500 bg-gray-100 shadow-md hover:shadow-xl delay-200 duration-300 ">
                 <div class="card-body">
                     <h2 class="card-title">{{ post.title }}</h2>
-                    <p class="text-sm"> By : <b class="text-indigo-500">{{ post.author }}</b> </p>
+                    <p class="text-sm"> {{ $t('by') }} : <b class="text-indigo-500">{{ post.author }}</b> </p>
                     <div class="card-actions justify-end">
                     </div>
                 </div>
