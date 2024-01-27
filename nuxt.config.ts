fcc63f44f2e047c7b3853e9551d29bc80892b6f4
Ashgan <ashganwiki@gmail.com>
2024-01-28 01:25:57 +0200

    update : package lock file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
-  tailwindcss: {
-    // Options
-  },
   i18n: {
     lazy: true,
     langDir: "locale",
